416fef772fb45cf715a550df8b4dbb6a37d5025b
revlinko <contact.revlinko@gmail.com>
2026-05-25 10:02:31 -0700

    error solve in topbar

--- src/componenets/Topbar.jsx
+++ src/componenets/Topbar.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
-import { BellDot, UserPlus, Search, Info, PanelRight,Power } from 'lucide-react';
+import { BellDot, UserPlus, Search, Info, PanelRight, Power } from 'lucide-react';
 import { useNavigate } from 'react-router';
-import { useUser } from './userContext';
+import { useUser } from './UserContext';
 
 
 const Topbar = ({ handleSideBar }) => {
-    const {user , setUser} = useUser();
+    const { user, setUser } = useUser();
     const [open, setopen] = useState(false)
     const navigate = useNavigate()
-    const handleLogout = () =>{
+    const handleLogout = () => {
         localStorage.removeItem("token");
         setUser(null);
         navigate('/login');
@@ -20,14 +20,14 @@ const Topbar = ({ handleSideBar }) => {
                 className="lg:hidden cursor-pointer text-gray-700 w-6 h-6"
 
             /> {console.log(user)}
-             {user && (<h1 className='text-indigo-700 font-medium text-xl'>hello... {user.name}</h1>)}
+            {user && (<h1 className='text-indigo-700 font-medium text-xl'>hello... {user.name}</h1>)}
             <div className='flex gap-5  shrink-0'>
                 <BellDot className="w-6 h-6 " />
                 <Info className="w-6 h-6" />
                 <UserPlus onClick={() => setopen(!open)} className="w-6 h-6" />
-                {open &&  (<button className='bg-red-200 rounded-full w-8 h-8 flex items-center justify-center text-red-700' onClick={handleLogout}>
-                    <Power className='w-6 h-6 '/>
-                </button> )}
+                {open && (<button className='bg-red-200 rounded-full w-8 h-8 flex items-center justify-center text-red-700' onClick={handleLogout}>
+                    <Power className='w-6 h-6 ' />
+                </button>)}
 
             </div>
         </div>
