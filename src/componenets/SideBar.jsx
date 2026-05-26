d8161b2b787983d0b21af0f0f97fde102cac6b35
revlinko <contact.revlinko@gmail.com>
2026-05-26 09:25:42 -0700

    add analytics route

--- src/componenets/SideBar.jsx
+++ src/componenets/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router";
-import {
+import {  
   LayoutDashboard,
   UserRoundSearch,
   Calendar,
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,44 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import { PieChart, Pie, Tooltip, Legend, Cell, } from 'recharts';
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#FF4842'];
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Tooltip, Legend, Cell } from "recharts";
 const BASE_URL = import.meta.env.VITE_API_URL;
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF4842"];
+
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
   const [total, setTotal] = useState(0);
 
   const fetchStatusCount = async () => {
-    const res = await fetch(`${BASE_URL}/leads/analytics/summary`);
-    const data = await res.json();
-    // console.log(data);
+    try {
+      const token = localStorage.getItem("token")
+      const res = await fetch(`${BASE_URL}/leads/analytics`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      });
+      const result = await res.json();
 
-    setData(data.byStatus.map((s) => ({
-      name: s._id, count: s.count
-    })))
-    setTotal(data.total);
-    // console.log(data.byStatus);
-    // console.log(data.total);
-  }
+      if (res.ok && result.success) {
+        setData(
+          result.byStatus.map((s) => ({
+            name: s._id || "No Status",
+            count: s.count,
+          }))
+        );
+        setTotal(result.total);
+      }
+      console.log(result.total)
+    } catch (err) {
+      console.log("Dashboard API error:", err);
+    }
+  };
 
   useEffect(() => {
     fetchStatusCount();
-  }, [])
+  }, []);
 
   return (
-    <div className='h-screen mt-10'>
-      <div className='flex gap-4 '>
-        <h2 className="font-bold text-2xl">Totle-leads : </h2>
-        <p className="bg-gray-100  rounded-full   px-2  border-2 border-gray-300">{total}</p>
+    <div className="min-h-screen mt-10">
+      <div className="flex gap-4">
+        <h2 className="font-bold text-2xl">Total Leads:</h2>
+        <p className="bg-gray-100 rounded-full px-2 border-2 border-gray-300">
+          {total}
+        </p>
       </div>
+
       <PieChart width={400} height={300}>
-        <Pie data={data} dataKey='count' nameKey='name' cx="50%" cy="50%" >
-          {data.map((_, i) => < Cell key={i} fill={COLORS[i % COLORS.length]} />)}
+        <Pie data={data} dataKey="count" nameKey="name" cx="50%" cy="50%">
+          {data.map((_, i) => (
+            <Cell key={i} fill={COLORS[i % COLORS.length]} />
+          ))}
         </Pie>
         <Tooltip />
         <Legend />
       </PieChart>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
